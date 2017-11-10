73d6602c92012126c707e99f295004395e21e870
Viktor <mac@macs-MacBook-Pro.local>
2017-11-11 02:27:59 +0300

    getting-started

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-app.use(express.logger());
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render('pages/index');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
